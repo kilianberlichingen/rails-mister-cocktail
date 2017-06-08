ba6aaa0f3605261373d841a7a48e455098c1e496
kilianberlichingen <kilian39@hotmail.de>
2017-06-08 18:24:40 +0200

    finish backend

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,31 @@
 class CocktailsController < ApplicationController
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
   def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save # Killing 2 birds with 1 stone
+      # Happy path
+      redirect_to cocktail_path(@cocktail)
+    else
+      # Sad sad path
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+  validates :cocktail, :description, :ingredient, presence: true
 end
