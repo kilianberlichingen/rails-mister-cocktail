bfa9d12f94e21256ce2af4db13ea3ece4def0552
kilianberlichingen <kilian39@hotmail.de>
2017-06-08 13:15:01 +0200

    add cocktail model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  root to: "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
